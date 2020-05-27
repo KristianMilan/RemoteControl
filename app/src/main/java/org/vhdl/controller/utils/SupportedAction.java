26c22e298e523c8ac4d02da34b17aa6bceec1ef2
luoxixi128 <eeluox@gmail.com>
2020-05-27 15:32:00 +0800

    整合版本remote controller

--- app/src/main/java/org/vhdl/controller/utils/SupportedAction.java
+++
@@ -1,7 +0,0 @@
-package org.vhdl.controller.utils;
-
-public class SupportedAction {
-    private static String[] actions = {"command"};
-
-    public static String[] getAllowed(){return actions;}
-}
